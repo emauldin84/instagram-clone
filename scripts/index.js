b157edf0eb42c4382de8618fa21ff0fbb123fdbd
Eric Mauldin <emauldin84@gmail.com>
2019-03-06 16:54:26 -0500

    adds div to display clicked image full-size. Hides large image when clicked again

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,43 @@
 // When we click an image
 // A const is a "constant" variable
 // Which means it cannot be reasssigned
-const targetElements = document.querySelectorAll("[data-target]");
+// const targetElements = document.querySelectorAll("[data-target]");
 
-// we see an enlarged version of
-// that image.
+// // we see an enlarged version of
+// // that image.
+
+// function respondToClick(event) {
+//     // add the "big" class to the thing that got clicked.
+//     // console.log(event.target.parentElement);
+//     event.target.parentElement.classList.toggle('big');
+// }
+
+// function attachClickHandler(oneElement) {
+//     oneElement.addEventListener("click", respondToClick);
+// }
+
+
+
+
+
+const targetElements = document.querySelectorAll('[data-target]');
+const largeImageOutput = document.querySelector('[data-output]');
 
 function respondToClick(event) {
-    // add the "big" class to the thing that got clicked.
-    console.log(event.target.parentElement);
-    event.target.parentElement.classList.toggle('big');
+    console.log(event.target.getAttribute('src'));
+    imgSrc = event.target.getAttribute('src');
+    largeImageOutput.setAttribute('src', imgSrc);
+    largeImageOutput.classList.remove('hidden');
 }
 
-function attachClickHandler(oneElement) {
-    oneElement.addEventListener("click", respondToClick);
-
+function attachClickEvent(eachImage) {
+    eachImage.addEventListener('click', respondToClick);
 }
 
-targetElements.forEach(attachClickHandler);
+function dismissLargeImage(event) {
+    largeImageOutput.classList.add('hidden');
+}
+
+largeImageOutput.addEventListener('click', dismissLargeImage)
+
+targetElements.forEach(attachClickEvent);
